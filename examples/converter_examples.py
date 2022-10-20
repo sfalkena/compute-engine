660c810b2030b801d780bd909b8cb15977ba0001
Sieger Falkena <siegerfalkena@hotmail.com>
2022-10-20 20:50:15 +0200

    Changes for publication

--- examples/converter_examples.py
+++ examples/converter_examples.py
@@ -3,14 +3,8 @@ import tensorflow as tf
 import larq_compute_engine as lce
 import larq_zoo as lqz
 
-# Example of converting a model from Larq Zoo to TF lite
-model = lqz.sota.QuickNet(weights="imagenet")
-converted = lce.convert_keras_model(model)
-with open("quicknet.tflite", "wb") as f:
-    f.write(converted)
-
 # Example of converting an h5 file
-model = tf.keras.models.load_model("my_model.h5")
+model = lqz.literature.BiRealNet(weights=PATH_TO_WEIGHTS_H5_FILE, num_classes=1000, lab_blocks=[True,True,True,True])          
 converted = lce.convert_keras_model(model)
-with open("my_model.tflite", "wb") as f:
-    f.write(converted)
+with open(TF_LITE_OUTPUT_FILE, "wb") as f:
+   f.write(converted)
